70f1766b983093547b61910746320549a8a633ce
st52550 <st52550@student.upce.cz>
2019-12-27 19:06:44 +0100

    layout

---
+++ code/index.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Josef Plášil
+ * Date: 4.11.19
+ * Time: 8:37
+ */
+
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="./style/layout.css">
+    <title>Najdi bydlení</title>
+</head>
+
+<body>
+    <header>
+        <div class="full-width-wrapper">
+            <img id="header-logo" src="./img/logo.png" width="128" height="128" alt="Logo">
+            <div id="header-title"><b>Najdibydlení.cz</b></div>
+            <div id="login-buttons">
+                <button>Přihlášení</button>
+                <button>Registrace</button>
+            </div>
+        </div>
+    </header>
+
+    <section id="nav-section">
+        <nav>
+            <button>Prvni</button>
+            <button>Druhy</button>
+            <button>Treti</button>
+        </nav>
+    </section>
+
+    <main>
+        <div id="sidenav">
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Clients</a>
+            <a href="#">Contact</a>
+        </div>
+
+        <div id="items">
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Fusce dui leo, imperdiet in, aliquam sit amet, feugiat eu, orci. Aliquam erat volutpat. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam bibendum elit eget erat. Nullam dapibus fermentum ipsum. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Etiam commodo dui eget wisi. Curabitur ligula sapien, pulvinar a vestibulum quis, facilisis vel sapien. Vivamus ac leo pretium faucibus. Etiam sapien elit, consequat eget, tristique non, venenatis quis, ante. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. Nulla accumsan, elit sit amet varius semper, nulla mauris mollis quam, tempor suscipit diam nulla vel leo. Nam quis nulla. Nunc tincidunt ante vitae massa.</p>
+
+            <p>Nulla non lectus sed nisl molestie malesuada. Nunc tincidunt ante vitae massa. Integer lacinia. Praesent dapibus. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Quisque tincidunt scelerisque libero. Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor. Nullam rhoncus aliquam metus. Integer in sapien. Fusce tellus odio, dapibus id fermentum quis, suscipit id erat. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris elementum mauris vitae tortor. Suspendisse sagittis ultrices augue. Aliquam in lorem sit amet leo accumsan lacinia. Curabitur ligula sapien, pulvinar a vestibulum quis, facilisis vel sapien.</p>
+
+            <p>Aenean fermentum risus id tortor. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Duis ante orci, molestie vitae vehicula venenatis, tincidunt ac pede. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. In dapibus augue non sapien. Nullam rhoncus aliquam metus. Aliquam erat volutpat. Suspendisse nisl. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Sed ac dolor sit amet purus malesuada congue.</p>
+
+            <p>Vivamus porttitor turpis ac leo. Praesent id justo in neque elementum ultrices. Fusce tellus. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Nullam at arcu a est sollicitudin euismod. Integer malesuada. Etiam neque. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Aliquam erat volutpat. Pellentesque arcu. Fusce wisi.</p>
+
+            <p>Praesent in mauris eu tortor porttitor accumsan. Suspendisse nisl. Mauris tincidunt sem sed arcu. Aenean vel massa quis mauris vehicula lacinia. Aliquam ornare wisi eu metus. In dapibus augue non sapien. Vestibulum fermentum tortor id mi. Nullam faucibus mi quis velit. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Pellentesque ipsum. Morbi imperdiet, mauris ac auctor dictum, nisl ligula egestas nulla, et sollicitudin sem purus in lacus. Phasellus rhoncus.</p>
+        </div>
+    </main>
+
+    <footer>
+        <div class="full-width-wrapper">
+            <div class="flex-wrap">
+                <section>
+                    <h4>Odkazy</h4>
+                    <ul>
+                        <li><a href="#">Domů</a> </li>
+                        <li><a href="#">Přihlášení</a> </li>
+                        <li><a href="#">Registrace</a> </li>
+                        <li><a href="#">Kontakt</a> </li>
+                    </ul>
+                </section>
+
+                <section>
+                    <p><b>©2020</b><a href="https://github.com"><b>Josef Plášil</b></a></p>
+                </section>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
